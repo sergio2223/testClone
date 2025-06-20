9e998f088bc4fdeb136e98058bb352604f4d572b
SergiyKrv <kireevsergiy7@gmail.com>
2025-06-20 12:08:07 +0300

    test

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(){
     std::cout << "Hello Git!" << std::endl;
-    std::cout << "Add some text. Commit it!" << std::endl;
+    // std::cout << "Add some text. Commit it!" << std::endl;
     return 0;
 }
 
